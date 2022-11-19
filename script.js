0d3cb9a8dab0423e29e695dcd92c40f2f676714e
oludare89 <azizoyekan@hotmail.com>
2022-11-19 14:24:18 +0000

    Created outline of Enemy class and animate function

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 800;
+document.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 800;
 
-class Game {
-    constructor(){
-        this.enemies = [];
-    }
-    update(){
+    class Game {
+        constructor(){
+            this.enemies = [];
+        }
+        update(){
 
-    }
-    draw(){
+        }
+        draw(){
 
+        }
+        #addNewEnemy(){
+
+        }
     }
-    #addNewEnemy(){
-        
+
+    class Enemy {
+        constructor(){
+
+        }
+        update(){
+
+        }
+        draw(){
+
+        }
     }
-}
+
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        requestAnimationFrame(animate);
+    }
+});

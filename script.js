f02f7c7edb786093b8ff9bfec2bc6803f871387d
oludare89 <azizoyekan@hotmail.com>
2022-11-21 22:07:16 +0000

    Can push new enemies into the enemies array

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ window.addEventListener('load', function(){
     class Game {
         constructor(){
             this.enemies = [];
+            this.#addNewEnemy();
+            console.log(this.enemies);
         }
         update(){
 
@@ -15,28 +17,31 @@ window.addEventListener('load', function(){
 
         }
         #addNewEnemy(){
-
+            this.enemies.push(new Enemy());
         }
     }
 
     class Enemy {
         constructor(){
-
+            this.x = 100;
+            this.y = 100;
+            this.width = 100;
+            this.height = 100;
         }
         update(){
-
+            this.x--;
         }
         draw(){
-
+            ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
 
+    const game = new Game();
     let lastTime = 1;
     function animate(timeStamp){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         const deltaTime = timeStamp - lastTime;
         lastTime = timeStamp;
-        console.log(deltaTime);
         requestAnimationFrame(animate);
     };
 

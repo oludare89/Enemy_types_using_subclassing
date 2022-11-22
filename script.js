3d885c2138d569dd2f338f4c1ec9c1232eabf03d
oludare89 <azizoyekan@hotmail.com>
2022-11-22 22:50:41 +0000

    Added the sprite animations to the Enemy parent class

--- script.js
+++ script.js
@@ -44,13 +44,24 @@ window.addEventListener('load', function(){
         constructor(game){
             this.game = game;
             this.markedForDeletion = false;
+            this.frameX = 0;
+            this.maxFrame = 5;
+            this.frameInterval = 100;
+            this.frameTimer = 0;
         }
         update(deltaTime){
             this.x -= this.vx * deltaTime;
             if (this.x < 0 - this.width) this.markedForDeletion = true;
+            if (this.frameTimer > this.frameInterval){
+                if (this.frameX < this.maxFrame) this.frameX++
+                else this.frameX = 0;
+                this.frameTImer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
         }
         draw(ctx){
-            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
     }
 
@@ -87,7 +98,7 @@ window.addEventListener('load', function(){
             this.y += Math.sin(this.angle) * this.curve;
             this.angle += 0.04;
         }
-        draw(){
+        draw(ctx){
             ctx.save();
             ctx.globalAlpha = 0.7;
             super.draw(ctx);
@@ -113,9 +124,13 @@ window.addEventListener('load', function(){
             super.update(deltaTime);
             this.y += this.vy * deltaTime;
             if (this.y > this.maxLength) this.vy *= -1;
-            if (this.y < 0 - this.height) this.markedForDeletion = true;
+            if (this.y < 0 - this.height * 2) this.markedForDeletion = true;
         }
         draw(ctx){
+            ctx.beginPath();
+            ctx.moveTo(this.x + this.width * 0.5, 0);
+            ctx.lineTo(this.x + this.width * 0.5, this.y + 10);
+            ctx.stroke();
             super.draw(ctx);
         }
     }
